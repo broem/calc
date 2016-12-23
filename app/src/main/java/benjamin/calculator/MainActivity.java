15ab301fb01ecfc7616c2705199e94bea2e87198
broem <wacbleach@gmail.com>
2016-12-23 02:58:15 -0500

    looks more like a calculator, removed double input boxes, buttons galor

--- app/src/main/java/benjamin/calculator/MainActivity.java
+++ app/src/main/java/benjamin/calculator/MainActivity.java
@@ -4,19 +4,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText operand1;
-    private EditText operand2;
+    private TextView operand1;
+//    private EditText operand2;
     private Button add;
     private Button sub;
     private Button mult;
     private Button div;
-    private  Button clr;
+    private Button clr;
+    private Button mod;
+    private Button posneg;
+    private  Button zero, one, two, three, four, five, six, seven, eight, nine;
+
     private TextView result;
 
     @Override
@@ -24,28 +27,98 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        operand1 = (EditText) findViewById(R.id.editOperand1);
-        operand2 = (EditText) findViewById(R.id.editOperand2);
+        operand1 = (TextView) findViewById(R.id.editOperand1);
+//        operand2 = (EditText) findViewById(R.id.editOperand2);
 
         add = (Button) findViewById(R.id.add);
         sub = (Button) findViewById(R.id.sub);
         mult = (Button) findViewById(R.id.mult);
         div = (Button) findViewById(R.id.div);
         clr = (Button) findViewById(R.id.clr);
+        mod = (Button) findViewById(R.id.mod);
+        zero = (Button) findViewById(R.id.zero);
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        posneg = (Button) findViewById(R.id.posneg);
+
         result = (TextView) findViewById(R.id.result);
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("0");
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("1");
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("2");
+            }
+        });
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("3");
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("4");
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("5");
+            }
+        });
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("6");
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("7");
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("8");
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.append("9");
+            }
+        });
+
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(operand1.getText().length() > 0 && operand2.getText().length() > 0) {
-                    double oper1 = Double.parseDouble(operand1.getText().toString());
-                    double oper2 = Double.parseDouble(operand2.getText().toString());
-
-                    double aResult = (oper1 + oper2);
-
-                    result.setText(Double.toString(aResult));
-                }
+                if(operand1.getText().length() > 0)
+                operand1.append("+");
                 else
                 Toast.makeText(MainActivity.this,"Need input",Toast.LENGTH_LONG).show();
             }
@@ -54,14 +127,8 @@ public class MainActivity extends AppCompatActivity {
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(operand1.getText().length() > 0 && operand2.getText().length() > 0) {
-                    double oper1 = Double.parseDouble(operand1.getText().toString());
-                    double oper2 = Double.parseDouble(operand2.getText().toString());
-
-                    double aResult = (oper1 - oper2);
-
-                    result.setText(Double.toString(aResult));
-                }
+                if(operand1.getText().length() > 0)
+                    operand1.append("-");
                 else
                     Toast.makeText(MainActivity.this,"Need input",Toast.LENGTH_LONG).show();
             }
@@ -70,14 +137,8 @@ public class MainActivity extends AppCompatActivity {
         mult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(operand1.getText().length() > 0 && operand2.getText().length() > 0) {
-                    double oper1 = Double.parseDouble(operand1.getText().toString());
-                    double oper2 = Double.parseDouble(operand2.getText().toString());
-
-                    double aResult = (oper1 * oper2);
-
-                    result.setText(Double.toString(aResult));
-                }
+                if(operand1.getText().length() > 0)
+                   operand1.append("*");
                 else
                     Toast.makeText(MainActivity.this,"Need input",Toast.LENGTH_LONG).show();
             }
@@ -86,14 +147,8 @@ public class MainActivity extends AppCompatActivity {
         div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(operand1.getText().length() > 0 && operand2.getText().length() > 0) {
-                    double oper1 = Double.parseDouble(operand1.getText().toString());
-                    double oper2 = Double.parseDouble(operand2.getText().toString());
-
-                    double aResult = (oper1 / oper2);
-
-                    result.setText(Double.toString(aResult));
-                }
+                if(operand1.getText().length() > 0)
+                    operand1.append("/");
                 else
                     Toast.makeText(MainActivity.this,"Need input",Toast.LENGTH_LONG).show();
             }
@@ -103,7 +158,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 operand1.setText("");
-                operand2.setText("");
                 result.setText("0.00");
                 operand1.requestFocus();
             }
